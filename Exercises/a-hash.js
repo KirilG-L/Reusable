72b246cc9cd444f7657337b9ac2a307ee7de7f3a
Kiril <kiril.gudim@gmail.com>
2019-12-08 19:26:04 +0200

    sdsdsd

--- Exercises/a-hash.js
+++ Exercises/a-hash.js
@@ -3,6 +3,11 @@ const phonebook = {
   marcus:  '+380445554433',
   victor:  '+030303030303030303',
   niccolo:  '+380445554433',
+
+
+
+
+
 };
 const findPhoneByName = name => phonebook[name];
 module.exports = { phonebook, findPhoneByName };
